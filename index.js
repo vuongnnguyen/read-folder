6c6b2983ccee957d71abd289c876623fe9e9da8a
Nguyen Quoc Vuong <54581418+vuongnnguyen@users.noreply.github.com>
2022-07-19 21:39:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
-document.getElementById("filepicker").addEventListener("change", function(event) {
-    let files = event.target.files;
-    for (let i=0; i<files.length; i++) {
-        let reader = new FileReader();
-        reader.readAsDataURL(files[i]);
-        reader.onload = function () {
-            let a = document.createElement('a');
-            a.href = reader.result;
-            a.download = files[i].name;
-            a.style.display = 'none';
-            document.body.append(a);
-            a.click();
-            a.remove();
+function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      let notification = new Notification("Hi there!");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          let notification = new Notification("Hi there!");
         }
-    };
-  }, false);
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
+
+  if ('serviceWorker' in navigator) {
+   // We need the service worker registration to check for a subscription
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    // Do we already have a push message subscription?
+    serviceWorkerRegistration.pushManager.getSubscription()
+      .then(function(subscription) {
+        // Enable any UI which subscribes / unsubscribes from
+        // push messages.
+        // const pushButton = document.querySelector('.js-push-button');
+        // pushButton.disabled = false;
+        console.log(subscription);
+        console.log(subscription);
+        if (!subscription) {
+          // We aren't subscribed to push, so set UI
+          // to allow the user to enable push
+          return;
+        }
+
+        // Keep your server in sync with the latest subscriptionId
+        // sendSubscriptionToServer(subscription);
+
+        // showCurlCommand(subscription);
+
+        // Set your UI to show they have subscribed for
+        // push messages
+        // pushButton.textContent = 'Disable Push Messages';
+        isPushEnabled = true;
+      })
+      .catch(function(err) {
+        window.Demo.debug.log('Error during getSubscription()', err);
+      });
+  });
+
+}
