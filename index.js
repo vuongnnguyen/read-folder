d5e2ec3bfa1243eb7cb45742dab12d495c6ea6a4
Nguyen Quoc Vuong <54581418+vuongnnguyen@users.noreply.github.com>
2022-07-19 22:01:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,37 +24,57 @@ function notifyMe() {
     // want to be respectful there is no need to bother them anymore.
   }
 
-  if ('serviceWorker' in navigator) {
-   // We need the service worker registration to check for a subscription
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // Do we already have a push message subscription?
-    serviceWorkerRegistration.pushManager.getSubscription()
-      .then(function(subscription) {
-        // Enable any UI which subscribes / unsubscribes from
-        // push messages.
-        // const pushButton = document.querySelector('.js-push-button');
-        // pushButton.disabled = false;
-        console.log(subscription);
-        console.log(subscription);
-        if (!subscription) {
-          // We aren't subscribed to push, so set UI
-          // to allow the user to enable push
-          return;
-        }
+//   if ('serviceWorker' in navigator) {
+//     console.log("dsd")
+//    // We need the service worker registration to check for a subscription
+//   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+//     // Do we already have a push message subscription?
+//     console.log("dsd")
+//     serviceWorkerRegistration.pushManager.getSubscription()
+//       .then(function(subscription) {
+//         // Enable any UI which subscribes / unsubscribes from
+//         // push messages.
+//         // const pushButton = document.querySelector('.js-push-button');
+//         // pushButton.disabled = false;
+//         console.log(subscription);
+//         console.log(subscription);
+//         if (!subscription) {
+//           // We aren't subscribed to push, so set UI
+//           // to allow the user to enable push
+//           return;
+//         }
 
-        // Keep your server in sync with the latest subscriptionId
-        // sendSubscriptionToServer(subscription);
+//         // Keep your server in sync with the latest subscriptionId
+//         // sendSubscriptionToServer(subscription);
 
-        // showCurlCommand(subscription);
+//         // showCurlCommand(subscription);
 
-        // Set your UI to show they have subscribed for
-        // push messages
-        // pushButton.textContent = 'Disable Push Messages';
-        isPushEnabled = true;
-      })
-      .catch(function(err) {
-        window.Demo.debug.log('Error during getSubscription()', err);
-      });
-  });
+//         // Set your UI to show they have subscribed for
+//         // push messages
+//         // pushButton.textContent = 'Disable Push Messages';
+//         isPushEnabled = true;
+//       })
+//       .catch(function(err) {
+//         window.Demo.debug.log('Error during getSubscription()', err);
+//       });
+//   })
+//   .catch(function(err) {
+//     window.Demo.debug.log('Error during getSubscription()', err);
+//   });
+
+// }
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+        console.log('Service Worker Registered!', reg);
+        reg.pushManager.getSubscription().then(function(sub) {
+          console.log('Subscription object: ', sub);
+        })
+        .catch(function(err) {
+            console.log('Service Worker registration failed: ', err);
+          });
+    })
 
 }
+    
